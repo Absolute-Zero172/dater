b2f5d66d307f3e4ef57cb9402571e0972b1e503f
Absolute-Zero172 <calvinjwasilevich@gmail.com>
2025-09-21 15:38:29 -0500

    add: added dater function

--- daterutils.py
+++ daterutils.py
@@ -23,3 +23,31 @@ def rename_file(old_name: str, new_name: str) -> None:
         print(f"Error: A file named '{new_name}' already exists.")
     except OSError as e:
         print(f"An operating system error occurred: {e}")
+
+
+def date_files(files: list[str], format_string="%Y.%m.%d", delimiter='--', pad_delimiter=True, titleize=True) -> None:
+    if pad_delimiter:
+        delimiter = f' {delimiter} '
+
+    for old_name in files:
+        # get date
+        modified_date = get_modified_date(old_name)
+        formatted_date = modified_date.strftime(format_string)
+
+        # get original name
+        original_name = old_name.split("\\")[-1]
+
+        # titleize
+        if titleize:
+            original_name = ('.'.join(original_name.split(".")[:-1])).title() + '.' + original_name.split(".")[-1]
+
+        # modify original name
+        modified_name = formatted_date + delimiter + original_name
+
+        # append path
+        path = "\\".join(old_name.split("\\")[:-1])
+        new_name = path + ("\\" if path != '' else '') + modified_name
+
+        # rename files
+
+        rename_file(old_name, new_name)
